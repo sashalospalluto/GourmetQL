5acb239350f8b4c285921b8581e1d214d2303795
SantiagoAmbricca <santiagoambricca@hotmail.com>
2025-09-21 14:12:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GourmetQL',
+  webDir: 'www'
+};
+
+export default config;
